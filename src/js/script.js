744e9fa7b4b08189eddc34b7e79421001f3fcf51
TaraldRotsaert <tarald.rotsaert@student.howest.be>
2018-11-02 18:08:29 +0100

    webpack and babelrc update for react

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './containers/App.jsx';
 
 {
     const init = () => {
         console.log('test');
+        ReactDOM.render(<App />, document.body.querySelector(`.react`));
     }
 
     init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,75 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require("webpack-merge");
+const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
 
+const PATHS = {
+  src: path.join(__dirname, "src"),
+  dist: path.join(__dirname, "dist")
+};
+
+const commonConfig = merge([
+  {
+    entry: [path.join(PATHS.src, "css/style.css"), path.join(PATHS.src, "js/script.js")],
+    output: {
+      path: PATHS.dist,
+      filename: `js/script.[hash].js`
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          use: [ 'babel-loader?cacheDirectory' ],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html"
+      })
+    ]
+  }
+]);
+
+const productionConfig = merge([
+  {
+    plugins: [
+      new CriticalPlugin({
+        src: 'index.html',
+        inline: true,
+        minify: true,
+        dest: 'index.html'
+      })
+    ]
+  }
+]);
+
+const developmentConfig = merge([
+  {
+    devServer: {
+      overlay: true,
+      contentBase: PATHS.src
+    }
+  }
+]);
+
+module.exports = env => {
+  if (process.env.NODE_ENV === "production") {
+    console.log("building production");
+    return merge(commonConfig, productionConfig);
+  }
+  return merge(commonConfig, developmentConfig);
+};
+
+/*
 module.exports = {
   entry: './src/js/script.js',
 
   output: {
-    filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.dist,
+    filename: `js/script.[hash].js`
   },
 
   module: {
@@ -34,3 +96,4 @@ module.exports = {
     }),
   ],
 };
+*/
